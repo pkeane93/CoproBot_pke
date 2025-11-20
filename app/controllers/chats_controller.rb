e2a8c9602dd4609180cab9ae92d46fd45bbb3ff8
Andreas Sousa Branca <andreaslsb@gmail.com>
2025-11-20 12:12:39 +0800

    started creating the create function of chats, not finalized yet

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -10,4 +10,18 @@ class ChatsController < ApplicationController
     @chats = @section.chats.order(created_at: :desc)
   end
   
+  def create
+    @section = Section.find_by(params[:id])
+
+    # initialize @chat variable with last chat
+    @chat = Chat.last
+    
+    # check if that chat alredy has some messages, if yes, then it is a used chat, hence we have to create a new one
+    if @chat.messages.any?
+      @chat = Chat.create!(user: current_user, section: @section, title: "New chat")
+    end
+
+    raise
+  end
+
 end
